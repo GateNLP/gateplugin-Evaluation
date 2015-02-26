8b0476ccc7e77e9dc81d26165bffd7f85909e9b1
Johann Petrak <johann.petrak@gmail.com>
2015-02-26 16:18:35 +0000

    Cleanup.

--- init.groovy
+++
@@ -1,6 +0,0 @@
-gate.Gate.init()
-import static gate.plugin.evaluation.tests.TestUtils.*;
-import gate.plugin.evaluation.api.*;
-import static gate.Utils.*;
-import gate.plugin.evaluation.resources.*;
-import gate.*;
