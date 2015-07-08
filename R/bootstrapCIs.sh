85b263192e4b78a53c8dda8b464f6332c03822b8
Johann Petrak <johann.petrak@gmail.com>
2015-07-08 21:59:45 +0200

    Add code for finding the script's own location.

--- R/bootstrapCIs.sh
+++ R/bootstrapCIs.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-Rscript  bootstrapCIs.r "$@" 
+PRG="$0"
+CURDIR="`pwd`"
+# need this for relative symlinks
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`"/$link"
+  fi
+done
+SCRIPTDIR=`dirname "$PRG"`
+SCRIPTDIR=`cd "$SCRIPTDIR"; pwd -P`
+ROOTDIR=`cd "$SCRIPTDIR"/..; pwd -P`
+
+Rscript --vanilla  $SCRIPTDIR/bootstrapCIs.r "$@" 
