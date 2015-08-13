5a61f06d577a6386e297d732f745ca8cdedf74f5
Johann Petrak <johann.petrak@gmail.com>
2015-08-13 22:05:06 +0200

    Another attempt to fix the NPE problem.

--- src/gate/plugin/evaluation/resources/EvaluateTagging4Lists.java
+++ src/gate/plugin/evaluation/resources/EvaluateTagging4Lists.java
@@ -549,7 +549,7 @@ public class EvaluateTagging4Lists extends EvaluateTaggingBase implements Contro
     
     ByRankEvalStatsTagging tmpEs = new ByRankEvalStatsTagging(ThresholdsOrRanksToUse.USE_RANKS_ALL);
     // find what the highest rank is over all the lists that do have a match for this document
-    int maxRankTh = -1;
+    int maxRankTh = 0; // always create a stats object for rank 0, if all else fails this will remain empty (no counts).
     for(CandidateList cl : candLists) {
       //System.out.println("Found a candidate list with candidates: "+cl.sizeAll());
       if(cl.sizeAll()-1 > maxRankTh) {
@@ -724,13 +724,8 @@ public class EvaluateTagging4Lists extends EvaluateTaggingBase implements Contro
     //System.out.println("<----------------- tmpEs");
     // per document we only output the stats for rank 0
     if(mainTsvPrintStream!=null) {
-      EvalStatsTagging tmpEST = tmpEs.get(0);
-      if(tmpEST != null) {
-        mainTsvPrintStream.println(outputTsvLine("list-disamb-best", document.getName(), typeSpec, 
+      mainTsvPrintStream.println(outputTsvLine("list-disamb-best", document.getName(), typeSpec, 
               responseSet.getName(), tmpEs.get(0)));
-      } else {
-        // TODO: should we log a warning here?
-      }
     }
     
     
