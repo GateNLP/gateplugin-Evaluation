07c66421294430de911502459f658ac070131ed0
Johann Petrak <johann.petrak@gmail.com>
2018-05-15 16:09:54 +0100

    Update license headers

--- src/main/java/gate/plugin/evaluation/EvaluateMaxRecall.java
+++ src/main/java/gate/plugin/evaluation/EvaluateMaxRecall.java
@@ -1,12 +1,21 @@
 /*
- *  Copyright (c) 1995-2015, The University of Sheffield. See the file
- *  COPYRIGHT.txt in the software or at http://gate.ac.uk/gate/COPYRIGHT.txt
- *
- *  This file is part of GATE (see http://gate.ac.uk/), and is free
- *  software, licenced under the GNU Library General Public License,
- *  Version 2, June 1991 (in the distribution as file licence.html,
- *  and also available at http://gate.ac.uk/gate/licence.html).
- *
+ * Copyright (c) 2015-2018 University of Sheffield.
+ * 
+ * This file is part of gateplugin-Evaluation 
+ * (see https://github.com/GateNLP/gateplugin-Evaluation).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package gate.plugin.evaluation.resources;
 
